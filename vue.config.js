4f6c03d078e468573e7655dbd13a5d1617d5e7aa
Ali Aref <3li3ref7777@gmail.com>
2026-09-15 19:22:23 +0200

    Update publicPath to mr-aref-admin and disable performance hints

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/mr-aref-admin/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/mr-aref-admin/' : '/',
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  }
 })
